c2ca8e44a42b9b0bab296b912270275d98a1b028
PJainGH <piyush.jain@capgemini.com>
2021-07-08 10:01:28 +0530

    emp class added with getter and setters

---
+++ demo/src/main/java/demo/Employee.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Employee {
+
+		int empl;
+		String empname;
+		public int getEmpl() {
+			return empl;
+		}
+		public void setEmpl(int empl) {
+			this.empl = empl;
+		}
+		public String getEmpname() {
+			return empname;
+		}
+		public void setEmpname(String empname) {
+			this.empname = empname;
+		}
+
+}
--- demo/src/main/java/demo/Hello_world.java
+++ demo/src/main/java/demo/Hello_world.java
@@ -5,6 +5,9 @@ public class Hello_world {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("wELCOME");
+		System.out.println("To");
+		System.out.println("GitHub Programming");
 
 	}
 
